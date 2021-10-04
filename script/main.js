63737687989d14e6658326f9ab9e367f82e33501
micheaol <micheaol80@gmail.com>
2021-10-05 00:02:39 +0100

    feat: add toggle function to the toggle menu

--- script/main.js
+++ script/main.js
@@ -2,6 +2,9 @@ const getMyElement = (param) => document.querySelector(param);
 
 const createMyElement = (param) => document.createElement(param);
 
+
+//Get navBar:
+const navBar = getMyElement(".nav-bar");
 const menuDiv = createMyElement("div");
 menuDiv.className = "toggle-nav";
 const menuUl = createMyElement("ul");
@@ -26,10 +29,31 @@ const hamburgerBtn = getMyElement(".hamburger-icon");
 
 hamburgerBtn.addEventListener("click", () => {
   document.body.appendChild(menuDiv);
+  navBar.style.display = "none";
   menuDiv.style.display = "block";
-  menuDiv.style.backgroundColor = "blue";
+  menuDiv.style.backgroundColor = "#3C3A39";
 });
 
 closeBtn.addEventListener("click", () => {
   menuDiv.style.display = "none";
+  navBar.style.display = "block";
 });
+
+menuPortfolio.addEventListener("click", ()=>{
+  menuDiv.style.display = "none";
+  document.location = "#projects-title";
+  navBar.style.display = "block";
+});
+
+menuAbout.addEventListener("click", ()=>{
+  menuDiv.style.display = "none";
+  document.location = "#about-section";
+  navBar.style.display = "block";
+});
+
+menuContact.addEventListener("click", ()=>{
+  menuDiv.style.display = "none";
+  document.location = "#contact-div";
+  navBar.style.display = "block";
+});
+
