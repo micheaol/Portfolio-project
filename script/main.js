b3b0c89dcd610b071f556304e38386b843642450
micheaol <micheaol80@gmail.com>
2021-10-04 19:54:51 +0100

    fix:make healines section more responsive

---
+++ script/main.js
@@ -0,0 +1,49 @@
+// //function to create Dom Element:
+// const createMyElement = (para) => {
+//     return document.createElement(para);
+// }
+
+// //Function to get element:
+// const getMyElement = (para)=>{
+//     return document.querySelector(para)
+// }
+
+// //Get Navbar:
+
+// const myNav = getMyElement('.nav-bar');
+// //create div for menu display:
+// const formDisplay = createMyElement('div');
+// formDisplay.id = "menu-hamburger";
+
+// //Create ul and lis:
+// const menuUl = createMyElement('ul');
+// const menuPortfolio = createMyElement('li');
+// menuPortfolio.textContent = "Portfolio"
+// const menuAbout = createMyElement('li');
+// menuAbout.textContent = 'About';
+// const menuContact = createMyElement('li');
+// menuContact.textContent = "Contact";
+
+// menuUl.appendChild(menuPortfolio)
+// menuUl.appendChild(menuAbout)
+// menuUl.appendChild(menuContact)
+
+// formDisplay.appendChild(menuUl)
+
+
+// //Get Hamburger button:
+// const hamburgerButton = getMyElement('.hamburger-icon');
+
+// //Add Event listener to the hambugerbutton:
+// hamburgerButton.addEventListener("click", ()=>{
+//     document.body.appendChild(formDisplay);
+//     // formDisplay.style.width = "100%";
+//     formDisplay.style.backgroundColor = 'red';
+//     myNav.style.display = 'none'
+//     formDisplay.style.display = 'block';
+// })
+
+
+
+
+
