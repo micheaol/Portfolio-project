8fd80885cf86d1c22655a47341778fa941737c0e
micheaol <micheaol80@gmail.com>
2021-10-06 21:57:51 +0100

    feat: add hover effects on the projects buttons to show when hover on, style the pop-up window

--- script/main.js
+++ script/main.js
@@ -126,51 +126,40 @@ menuContact.addEventListener('click', () => {
 
 function displayProjects() {
   projects.forEach((project) => {
-    //Create a div:
+
     const projectDiv = createMyElement('div');
     projectDiv.className = 'job-item-with-btn';
 
-    //create another div to hold the title, summary,image and tech skills
     const jobDiv = createMyElement('div');
     jobDiv.className = 'job-item-two';
 
-    //create H1 for the title:
     const jobTitle = createMyElement('h1');
     jobTitle.className = 'job-post-title';
     jobTitle.textContent = project.title;
 
-    //append h1 to the jobDiv:
     jobDiv.appendChild(jobTitle);
 
-    //create another div to hold summary:
     const sumaDiv = createMyElement('div');
     sumaDiv.className = 'job-post';
 
-    //Create a paragraphe to append to the summaDiv:
     const para = createMyElement('p');
     para.textContent = project.tips;
 
-    //append para to job post div:
     sumaDiv.appendChild(para);
 
-    //append SumaDiv to jobDiv:
     jobDiv.appendChild(sumaDiv);
 
-    //create a div to hold the technologies:
     const techDiv = createMyElement('div');
     techDiv.className = 'job-tech-lang';
 
-    //Create a ul to hold the technologies:
     const ulTech = createMyElement('ul');
     ulTech.className = 'job-tech-lang-ul';
 
-    //Create an anchor button:
     const seeProjectBtn = createMyElement('a');
     seeProjectBtn.className = 'long-link see-jobs-btn';
     seeProjectBtn.href = '#';
     seeProjectBtn.textContent = 'See Project';
 
-    //Create lis to pass into the ul:
     for (let counter = 0; counter < project.techologies.length; counter += 1) {
       let jobLi = createMyElement('li');
       jobLi.className = 'job-item job-one';
@@ -178,14 +167,12 @@ function displayProjects() {
       ulTech.appendChild(jobLi);
     }
 
-    //append ul tech to techDiv:
     techDiv.appendChild(ulTech);
 
     jobDiv.appendChild(techDiv);
     projectDiv.appendChild(jobDiv);
     projectDiv.appendChild(seeProjectBtn);
 
-    //we add event listener to the button:
     seeProjectBtn.addEventListener('click', () => {
       const popUpContainer = createMyElement('div');
       popUpContainer.className = 'pop-up-container';
@@ -216,7 +203,6 @@ function displayProjects() {
 
       const popUpImg = createMyElement('img');
       popUpImg.className = 'pop-up-img';
-      // popUpImg.src = project.projectImg;
 
       popUpInnerImgDiv.style.backgroundImage =
         'url(' + project.projectImg + ')';
